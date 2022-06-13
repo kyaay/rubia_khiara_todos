04ea26bdfadd6ed69596d4a6a5ad44a2fb81778f
Khiara Danielle Rubia <khiara.rubia@usjr.edu.ph>
2022-06-14 02:22:28 +0800

    Todo App part 3

--- todo_app/lib/src/app.dart
+++ todo_app/lib/src/app.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:todo_app/src/screens/home.dart';
+import 'package:todo_app/src/screens/login/auth_screen.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      home: Wrapper(),
     );
   }
 }
